a3a0ad2434f4b91c5e51901c633ed87216f72d61
mckema01 <Matthew.Mckeller@lexisnexisrisk.com>
2019-07-12 11:54:48 -0400

    Add pagination

--- src/graphql-prisma/resolvers/Query.js
+++ src/graphql-prisma/resolvers/Query.js
@@ -1,13 +1,17 @@
 import getUserId from '../utils/get-user-id';
 
 const Query = {
-    users(parent, args, {prisma}, info) {
-        const opArgs = {};
+    users(parent, {query, first, skip, after}, {prisma}, info) {
+        const opArgs = {
+            first,
+            skip,
+            after
+        };
 
-        if (args.query) {
+        if (query) {
             opArgs.where = {
                 OR: [{
-                    name_contains: args.query
+                    name_contains: query
                 }]
             };
         }
@@ -19,9 +23,12 @@ const Query = {
 
         return prisma.query.user({where: {id: userId}}, info);
     },
-    myPosts(parent, {query}, {request, prisma}, info) {
+    myPosts(parent, {query, first, skip, after}, {request, prisma}, info) {
         const userId = getUserId(request);
         const opArgs = {
+            first,
+            skip,
+            after,
             where: {
                 author: {
                     id: userId
@@ -39,28 +46,35 @@ const Query = {
 
         return prisma.query.posts(opArgs, info);
     },
-    posts(parent, args, {prisma}, info) {
+    posts(parent, {query, first, skip, after}, {prisma}, info) {
         const opArgs = {
+            first,
+            skip,
+            after,
             where: {
                 published: true,
             }
         };
 
-        if (args.query) {
+        if (query) {
             opArgs.where.OR = [{
-                title_contains: args.query
+                title_contains: query
             }, {
-                body_contains: args.query
+                body_contains: query
             }]
         }
 
         return prisma.query.posts(opArgs, info);
     },
-    comments(parent, args, {db, prisma}, info) {
-        const opArgs = {};
-        if (args.query) {
+    comments(parent, {query, first, skip, after}, {db, prisma}, info) {
+        const opArgs = {
+            first,
+            skip,
+            after,
+        };
+        if (query) {
             opArgs.where = {
-                text_contains: args.query
+                text_contains: query
             }
         }
 
